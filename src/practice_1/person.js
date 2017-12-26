3aa17fe704ab00e04233dab24e8ccff9e8e3d193
chiu <865533962@qq.com>
2017-12-26 11:49:14 +0800

    person

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,10 +1,8 @@
 class Person {
-
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-
     introduce() {
         return `My name is ${this.name}. I am ${this.age} years old.`;
     }
