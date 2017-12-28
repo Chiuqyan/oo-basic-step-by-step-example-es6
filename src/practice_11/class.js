3b879a016ceda25b7bbb1d84f1ed02f086abf93b
chiu <865533962@qq.com>
2017-12-28 20:37:36 +0800

    changed

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,32 @@
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
 
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student, teacher) {
+        if (student.klass.number === this.number) {
+            this.leader = student;
+            if (typeof(teacher) !== 'undefined')
+                teacher.registerAssignLeaderListener(student);
+        } else
+            console.log("It is not one of us.");
+    }
+
+    appendMember(student, teacher) {
+        student.klass = this;
+        if (typeof(teacher) !== 'undefined')
+            teacher.registerJoinListener(student);
+    }
+    isIn(student) {
+        if (student.klass.number === this.number)
+            return true;
+        else
+            return false;
+    }
+}
+exports["default"] = Class;
+module.exports = exports["default"];
